431512efee705a29a5f2569490a6d1008a190490
Lupino <lmjubuntu@gmail.com>
2019-10-21 17:08:05 +0800

    set auto reboot false

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 extern "C" {
 #endif
 #define DEBUG 0
-#define AUTO_REBOOT 1 // enable auto reboot
+#define AUTO_REBOOT 0 // enable auto reboot
 #define REBOOT_TIMER 6 * 60 * 60 * 1000 // reboot every 6 hours
 #ifdef __cplusplus
 }
