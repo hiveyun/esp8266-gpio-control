2e7dd0e63b6573f129f50dbf32be61a7a6dee3cb
Lupino <lmjubuntu@gmail.com>
2019-11-05 14:06:05 +0800

    set KEEP_SWITCH_STATE 1

--- config.h
+++ config.h
@@ -4,13 +4,15 @@
 extern "C" {
 #endif
 #define DEBUG 0
+
+
 #define AUTO_REBOOT 0 // enable auto reboot
 #define REBOOT_TIMER 6 * 60 * 60 * 1000 // reboot every 6 hours
 #define MAX_QUEUE_LENGTH 50
 
 // switch state config
 // lose power will keep the state
-#define KEEP_SWITCH_STATE 0
+#define KEEP_SWITCH_STATE 1
 #define SWITCH_STATE_ON 1
 #define SWITCH_STATE_OFF 0
 // set default switch state when started if no keep switch state
